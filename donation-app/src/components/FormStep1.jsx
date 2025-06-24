71a52738ebecf8eb085b5a6ac8b59c323894eeef
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-06-24 14:01:24 +0100

    Added appropriate forms to FormStep1 component

--- donation-app/src/components/FormStep1.jsx
+++ donation-app/src/components/FormStep1.jsx
@@ -1,5 +1,6 @@
 import Input from "./Input";
 import Button from "./Button";
+import styled from "styled-components";
 
 function FormStep1({ formData, onNext, onChange }) {
 
@@ -10,19 +11,51 @@ function FormStep1({ formData, onNext, onChange }) {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <Wrapper onSubmit={handleSubmit}>
+                <label for="orgName">Organization Name</label>
                 <Input 
                     name = "orgName"
                     placeholder = 'Organization name here'
                     value = {formData.orgName}
                     type = 'text'
-                    onChange = {(event) => onChange('orgName', event.target.value)}
+                    onChange = {(e) => onChange('orgName', e.target.value)}
+                />
+
+                <label for="orgEmail">Organization Email</label>
+                <Input 
+                    name="orgEmail"
+                    placeholder="Organization email here"
+                    value={formData.orgEmail}
+                    type = "email"
+                    onChange = {(e) => onChange('orgEmail', e.target.value)}
+                />
+
+                <label>Password</label>
+                <Input 
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    type="password"
+                    onChange={(e) => onChange('password', e.target.value)}
                 />
 
                 <Button>Next Step</Button>
-            </form>
+            </Wrapper>
         </>
     )
 }
+
+const Wrapper = styled.form `
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin: 10px;
+
+    & > Input {
+        margin-bottom: 10px;
+    }
+`;
+
+
 
 export default FormStep1;
--- donation-app/src/components/FormStep3.jsx
+++ donation-app/src/components/FormStep3.jsx
@@ -1,1 +1,28 @@
 import Input from "./Input";
+import Button from "./Button";
+
+function FormStep3({ formData, onNext, onChange }) {
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        onNext();
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <Input 
+                    name = "orgName"
+                    placeholder = 'Organization name here'
+                    value = {formData.orgName}
+                    type = 'text'
+                    onChange = {(event) => onChange('orgName', event.target.value)}
+                />
+
+                <Button>Next Step</Button>
+            </form>
+        </>
+    )
+}
+
+export default FormStep3;
